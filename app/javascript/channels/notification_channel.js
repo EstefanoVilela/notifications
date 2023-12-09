c288f4ef6e756e94be53424e92d55435b4a0ade7
EstefanoVilela-Geosatelital <evilela@geosatelital.com.pe>
2023-12-08 23:29:06 -0500

    push notifications

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -2,14 +2,35 @@ import consumer from "channels/consumer"
 
 consumer.subscriptions.create("NotificationChannel", {
   connected() {
-    console.log("connected");
+    if ('Notification' in window && 'serviceWorker' in navigator) {
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          console.log('Conectado y con permiso para notificaciones.');
+        } else {
+          console.warn('Conectado y sin permiso para notificaciones.');
+        }
+      });
+    } else {
+      console.log('Las notificaciones push no son compatibles en este navegador.');
+    }
   },
 
   disconnected() {
     console.log("disconnected");
   },
 
-  received(data) {
-    console.log({ data });
+  received(title) {
+    if (Notification.permission === 'granted') {
+      const options = {
+        body: title,
+        // icon: 'ruta/a/la/imagen.png'
+      };
+
+      const notificacion = new Notification('Aviso!', options);
+
+      notificacion.onclick = function () {
+        console.log('La notificación fue clicada.');
+      };
+    }
   }
 });
